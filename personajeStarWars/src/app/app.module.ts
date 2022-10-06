f07d154fab641736e684d7c0b0df78b23b4686ec
ManuelCaroDam <caro.deman20@triana.salesianos.edu>
2022-10-06 10:57:23 +0200

    cambios

--- personajeStarWars/src/app/app.module.ts
+++ personajeStarWars/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CharacterListComponent } from './components/character-list/character-list.component';
 import { MaterialImportsModule } from './modules/material-imports.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { MaterialImportsModule } from './modules/material-imports.module';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    MaterialImportsModule
+    MaterialImportsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
